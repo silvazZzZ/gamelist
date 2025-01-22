5341a6c646705ee1db1e22527a80a245bb256ba5
silvazZzZ <irivamjunior717@gmail.com>
2025-01-22 20:13:03 -0300

    Properties, Game, ORM

--- src/main/java/com/gamelist/gamelist/GamelistApplication.java
+++ src/main/java/com/gamelist/gamelist/GamelistApplication.java
@@ -2,8 +2,10 @@ package com.gamelist.gamelist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.gamelist.entities")
 public class GamelistApplication {
 
 	public static void main(String[] args) {
